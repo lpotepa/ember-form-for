1c1578b712dc82dd2b339cac6200789bac786225
Łukasz Potępa <lpotepa@o2.pl>
2017-01-06 20:47:06 +0100

    Test2

--- app/instance-initializers/form-for-initializer.js
+++ app/instance-initializers/form-for-initializer.js
@@ -4,7 +4,6 @@ import config from '../config/environment';
 const { merge, set } = Ember;
 
 export function initialize(application) {
-  let formForConfig = merge(DEFAULT_CONFIG, config['ember-form-for']);
   let configService = application.lookup('service:ember-form-for/config');
 
   Object.keys(formForConfig).forEach((key) => {
