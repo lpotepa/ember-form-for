839a0495880a207f5e1e416dce30c6b59407368e
Łukasz Potępa <lpotepa@o2.pl>
2017-01-07 19:26:30 +0100

    test

--- addon/components/form-fields/url-field.js
+++ addon/components/form-fields/url-field.js
@@ -1,7 +1,7 @@
 import TextField from './text-field';
 
 export default TextField.extend({
-  control: 'one-way-url'
+  control: 'one-way-url',
   id: Ember.computed('propertyName', function(){
     return this.get('propertyName');
   }),
