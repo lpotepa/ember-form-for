f87b7ec591599d3add80f6f68739916ae204311d
Łukasz Potępa <lpotepa@o2.pl>
2017-01-07 19:25:32 +0100

    test

--- addon/components/form-fields/color-field.js
+++ addon/components/form-fields/color-field.js
@@ -1,7 +1,7 @@
 import TextField from './text-field';
 
 export default TextField.extend({
-  control: 'one-way-color'.
+  control: 'one-way-color',
   id: Ember.computed('propertyName', function(){
     return this.get('propertyName');
   }),
